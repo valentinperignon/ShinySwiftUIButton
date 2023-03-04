247a107be20aadb13ea1412d68d86e84f0ef6753
Valentin Perignon <valentin.perignon@icloud.com>
2023-03-04 18:27:51 +0100

    Initial Commit

---
+++ Shiny Button/Shiny_ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shiny_ButtonApp.swift
+//  Shiny Button
+//
+//  Created by Valentin Perignon on 04/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Shiny_ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
