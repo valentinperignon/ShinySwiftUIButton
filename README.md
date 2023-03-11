089a0588f137703c0c1cf99e57282dc6eece5cc1
Valentin Perignon <valentin.perignon@icloud.com>
2023-03-11 23:37:07 +0100

    feat: Reproduce button

---
+++ README.md
@@ -0,0 +1,4 @@
+# Shiny Button
+
+Made with SwiftUI.
+Inspired by the work of [Alex Widua](https://shiny-button.vercel.app/)
--- Shiny Button/ContentView.swift
+++ Shiny Button/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color("backgroundColor")
+                .ignoresSafeArea()
+
+            Button(action: {}) {
+                Text("Button")
+                    .font(.system(size: 48, weight: .medium))
+                    .foregroundColor(Color("textColor"))
+                    .padding(.horizontal, 72)
+                    .padding(.vertical, 24)
+            }
+            .background(
+                RoundedRectangle(cornerRadius: 56)
+                    .fill(
+                        .shadow(.inner(color: .white, radius: 2, x: 0, y: 2))
+                        .shadow(.inner(color: .white.opacity(0.8), radius: 2, x: 0, y: 2))
+                        .shadow(.inner(color: .white.opacity(0.5), radius: 2, x: 0, y: -2))
+                    )
+                    .foregroundColor(Color("backgroundColor"))
+            )
+            .cornerRadius(56)
+            .shadow(color: .black.opacity(0.25), radius: 8, x: 0, y: 4)
+            .shadow(color: .black.opacity(0.15), radius: 32, x: 0, y: 16)
+            .shadow(color: .black.opacity(0.13), radius: 32, x: 0, y: 16)
         }
-        .padding()
     }
 }
 
