b7d3dc664a5df93d2795557b82d403ca4f0e16d9
Valentin Perignon <valentin.perignon@icloud.com>
2023-03-12 01:02:37 +0100

    feat: Pressed effect (v1)

--- Shiny Button/ContentView.swift
+++ Shiny Button/ContentView.swift
@@ -7,6 +7,50 @@
 
 import SwiftUI
 
+struct ShinyButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.system(size: 48, weight: .medium))
+            .foregroundColor(Color("textColor"))
+            .padding(.horizontal, 72)
+            .padding(.vertical, 24)
+            .background(
+                background(isPressed: configuration.isPressed)
+            )
+            .scaleEffect(configuration.isPressed ? 0.95 : 1)
+            .animation(.easeOut, value: configuration.isPressed)
+    }
+    
+    @ViewBuilder private func background(isPressed: Bool) -> some View {
+        if isPressed {
+            RoundedRectangle(cornerRadius: 56)
+                .fill(
+                    .shadow(.inner(color: .black.opacity(0.1), radius: 8, x: 0, y: 4))
+                    .shadow(.inner(color: .black.opacity(0.2), radius: 4, x: 0, y: 4))
+                    .shadow(.inner(color: .black.opacity(0.1), radius: 8, x: 0, y: -6))
+                    .shadow(.drop(color: .white.opacity(0.5), radius: 1, x: 0, y: -2))
+                    .shadow(.drop(color: .black.opacity(0.5), radius: 1, x: 0, y: -1))
+                    .shadow(.drop(color: .white.opacity(0.5), radius: 1, x: 0, y: -4))
+                    .shadow(.drop(color: .white.opacity(0.5), radius: 1, x: 0, y: 4))
+                    .shadow(.drop(color: .black.opacity(0.5), radius: 1, x: 0, y: 1))
+                    .shadow(.drop(color: .white.opacity(0.5), radius: 1, x: 0, y: 4))
+                )
+                .foregroundStyle(.ultraThinMaterial)
+        } else {
+            RoundedRectangle(cornerRadius: 56)
+                .fill(
+                    .shadow(.inner(color: .white, radius: 2, x: 0, y: 2))
+                    .shadow(.inner(color: .white.opacity(0.8), radius: 2, x: 0, y: 2))
+                    .shadow(.inner(color: .white.opacity(0.5), radius: 2, x: 0, y: -2))
+                    .shadow(.drop(color: .black.opacity(0.25), radius: 8, x: 0, y: 4))
+                    .shadow(.drop(color: .black.opacity(0.15), radius: 32, x: 0, y: 16))
+                    .shadow(.drop(color: .black.opacity(0.13), radius: 32, x: 0, y: 16))
+                )
+                .foregroundStyle(.ultraThinMaterial)
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         ZStack {
@@ -15,24 +59,8 @@ struct ContentView: View {
 
             Button(action: {}) {
                 Text("Button")
-                    .font(.system(size: 48, weight: .medium))
-                    .foregroundColor(Color("textColor"))
-                    .padding(.horizontal, 72)
-                    .padding(.vertical, 24)
             }
-            .background(
-                RoundedRectangle(cornerRadius: 56)
-                    .fill(
-                        .shadow(.inner(color: .white, radius: 2, x: 0, y: 2))
-                        .shadow(.inner(color: .white.opacity(0.8), radius: 2, x: 0, y: 2))
-                        .shadow(.inner(color: .white.opacity(0.5), radius: 2, x: 0, y: -2))
-                    )
-                    .foregroundColor(Color("backgroundColor"))
-            )
-            .cornerRadius(56)
-            .shadow(color: .black.opacity(0.25), radius: 8, x: 0, y: 4)
-            .shadow(color: .black.opacity(0.15), radius: 32, x: 0, y: 16)
-            .shadow(color: .black.opacity(0.13), radius: 32, x: 0, y: 16)
+            .buttonStyle(ShinyButtonStyle())
         }
     }
 }
